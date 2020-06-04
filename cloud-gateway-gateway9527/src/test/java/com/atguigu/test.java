1e69c4b864bcbaa91d68082aa278b3560df30a5a
songchao <songchao_ss@163.com>
2020-06-04 16:54:39 +0800

    finish spring cloud gateway and create spring cloud stream

---
+++ cloud-gateway-gateway9527/src/test/java/com/atguigu/test.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+import java.time.ZonedDateTime;
+
+public class test {
+
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+}
