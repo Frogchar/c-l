77d9637fa4b761e5977ee50c992ca813a3227dca
songchao <songchao_ss@163.com>
2020-06-08 16:34:48 +0800

    finished sentinel

---
+++ cloudalibaba-sentinel-server8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FloLimitController.java
@@ -0,0 +1,48 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@Slf4j
+public class FloLimitController {
+    @GetMapping("/testA")
+    public String testA()
+    {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB()
+    {
+        return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD()
+    {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD==============>");
+        return "------testD";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false)String p1,
+                             @RequestParam(value = "p2", required = false)String p2) {
+        return "test hotkey";
+    }
+
+    public String deal_testHotKey() {
+        return "-------deal_testHotKey----------/(ㄒoㄒ)/~~";
+    }
+}
---
+++ cloudalibaba-sentinel-server8401/src/main/java/com/atguigu/springcloud/alibaba/controller/RateLimitController.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.atguigu.springcloud.alibaba.myhandler.CustomerBlockHandler;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class RateLimitController {
+
+    @GetMapping("/rateLimit/customerBlockHandler")
+    @SentinelResource(value = "customerBlockHandler",blockHandlerClass = CustomerBlockHandler.class,
+                      blockHandler = "handlerException2")
+    public CommonResult customerBlockHandler() {
+        return new CommonResult(200, "按客戶自定义", new Payment(2020L, "serial003"));
+    }
+}
